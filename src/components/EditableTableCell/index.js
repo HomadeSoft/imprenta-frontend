c5b588b9c076ea31274d619238fcac6137fea01c
Juan Cruz Riera <juancriera94@gmail.com>
2023-05-15 17:13:48 -0300

    Merged conflicts after update

--- src/components/EditableTableCell/index.js
+++ src/components/EditableTableCell/index.js
@@ -1,76 +1,31 @@
 import React, { useState } from "react";
-import { TableCell, IconButton, TextField } from "@mui/material";
-import SaveIcon from "@mui/icons-material/Save";
-import CancelIcon from "@mui/icons-material/Cancel";
-import EditIcon from "@mui/icons-material/Edit";
+import { TextField } from "@mui/material";
+
 import PropTypes from "prop-types";
-import MDTypography from "components/MDTypography";
 
-function EditableTableCell({ value, color, onSave }) {
-  const [isEditing, setIsEditing] = useState(false);
+function EditableTableCell({ value, onBlur }) {
   const [tempValue, setTempValue] = useState(value);
-
-  const handleEditClick = () => {
-    setIsEditing(true);
-  };
-
-  const handleCancelClick = () => {
-    setIsEditing(false);
-    setTempValue(value);
-  };
-
-  const handleSaveClick = () => {
-    setIsEditing(false);
-    onSave(tempValue);
-  };
 
   const handleInputChange = (event) => {
     setTempValue(event.target.value);
   };
 
   return (
-    <TableCell>
-      {isEditing ? (
-        <>
-          <TextField
-            autoFocus
-            value={tempValue}
-            onChange={handleInputChange}
-            inputProps={{
-              style: { padding: 0 },
-            }}
-          />
-          <IconButton size="small" onClick={handleSaveClick}>
-            <SaveIcon />
-          </IconButton>
-          <IconButton size="small" onClick={handleCancelClick}>
-            <CancelIcon />
-          </IconButton>
-        </>
-      ) : (
-        <>
-          <MDTypography component="a" href="#" variant="button" color={color} fontWeight="medium">
-            {tempValue}
-          </MDTypography>
-          <IconButton size="small" onClick={handleEditClick}>
-            <EditIcon />
-          </IconButton>
-        </>
-      )}
-    </TableCell>
+    <TextField
+      value={tempValue}
+      onChange={handleInputChange}
+      onBlur={onBlur}
+    />
   );
 }
 
 EditableTableCell.defaultProps = {
   value: "",
-  onSave: () => { },
-  color: "text",
 };
 
 EditableTableCell.propTypes = {
   value: PropTypes.string,
-  onSave: PropTypes.func,
-  color: PropTypes.string,
+  onBlur: PropTypes.func,
 };
 
 export default EditableTableCell;
--- src/layouts/precios/index.js
+++ src/layouts/precios/index.js
@@ -1,85 +1,135 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import SaveIcon from "@mui/icons-material/Save";
+import EditIcon from "@mui/icons-material/Edit"
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import PriceTable from "examples/Tables/PriceTable";
 import bajadasSinPapel from "layouts/precios/data/bajadasSinPapel";
-import {Accordion, AccordionDetails, AccordionSummary} from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Fab, TextField } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Wrapper from "layouts/Wrapper";
+import EditableTableCell from "components/EditableTableCell";
 
 function Precios() {
-  const categorias = bajadasSinPapel(false, "nuevo");
-
+  let initialState = bajadasSinPapel();
   const [expanded, setExpanded] = useState("");
-
+  const [categorias, setCategorias] = useState(initialState);
+  const [editing, setEditing] = useState(false);
   const handleChange = (panel) => (event, newExpanded) => {
     setExpanded(newExpanded ? panel : false);
   };
+
+  const handleUpdate =
+    (categoryIndex, paperIndex, quantityIndex, optionsIndex) =>
+      (event) => {
+        let categoriasNew = [...categorias];
+        categoriasNew[categoryIndex].papel[paperIndex].quantities[quantityIndex].options[optionsIndex].value = event.target.value;
+        setCategorias([...categorias]);
+      };
+
   var lista = [];
-  categorias.forEach(categoria => {
-    const title = <MDTypography>{categoria.printSize}</MDTypography>;
-    var papel = [];
-    categoria.papel.forEach(elemento => {
-      var rows = [];
-      const columns = [];
-      elemento.quantities.forEach(quantity => {
-        var row = {};
-        row["cantidad"] = (<MDTypography variant="button" color="text" fontWeight="regular">{quantity.min}-{quantity.max}</MDTypography>);
-        quantity.options.forEach(option => {
-          row[option.description] = (<MDTypography variant="button" color="text" fontWeight="bold">{option.value}</MDTypography>);
+  var categoryIndex = 0;
+
+  if (categorias) {
+
+    categorias.forEach(categoria => {
+      const title = <MDTypography>{categoria.printSize}</MDTypography>;
+      var papel = [];
+      var paperIndex = 0;
+      categoria.papel.forEach(elemento => {
+        var rows = [];
+        const columns = [];
+        var quantityIndex = 0;
+        elemento.quantities.forEach(quantity => {
+          var row = {};
+          row["cantidad"] = (<MDTypography variant="button" color="text" fontWeight="regular">{quantity.min}-{quantity.max}</MDTypography>);
+          var optionsIndex = 0;
+          quantity.options.forEach(option => {
+            row[option.description] = (editing ? <EditableTableCell value={option.value} onBlur={handleUpdate(categoryIndex, paperIndex, quantityIndex, optionsIndex)} /> : <MDTypography variant="button" color="text" fontWeight="bold">{option.value}</MDTypography>);
+            optionsIndex++;
+          });
+          quantityIndex++;
+          rows.push(row)
         });
+        Object.keys(rows[0]).forEach(key => {
+          columns.push({ Header: key, accessor: key, align: "center" });
+        })
 
-        rows.push(row)
+        papel.push(<Accordion expanded={expanded === elemento.gramaje + categoria.printSize} onChange={handleChange(elemento.gramaje + categoria.printSize)}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+          >
+            <MDTypography sx={{ width: '33%', flexShrink: 0 }}>
+              {elemento.gramaje === null ? "sin papel" : (elemento.gramaje)}
+            </MDTypography>
+            <MDTypography sx={{ width: '33%', flexShrink: 0 }} fontWeight={"light"} color={"secondary"}>
+              {
+                elemento.caracteristicas.map((caracteristica) => "[" + caracteristica + "] ")
+              }
+            </MDTypography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <PriceTable
+              table={{ columns: columns, rows: rows }}
+              isSorted={false}
+              entriesPerPage={false}
+              showTotalEntries={false}
+              noEndBorder
+            />
+          </AccordionDetails>
+        </Accordion>);
+
+        paperIndex++;
       });
-      Object.keys(rows[0]).forEach(key => {
-        columns.push({Header: key, accessor: key, align: "center"});
-      })
-
-      papel.push(<Accordion expanded={expanded === elemento.gramaje + categoria.printSize}
-                            onChange={handleChange(elemento.gramaje + categoria.printSize)}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon/>}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <MDTypography sx={{width: '33%', flexShrink: 0}}>
-            {elemento.gramaje === null ? "sin papel" : (elemento.gramaje)}
-          </MDTypography>
-          <MDTypography sx={{width: '33%', flexShrink: 0}} fontWeight={"light"} color={"secondary"}>
+      lista.push(
+        <MDBox>
+          <MDBox mx={2}
+            mt={3}
+            py={3}
+            px={2}>
+            <MDTypography variant="h6">
+              {title}
+            </MDTypography>
             {
-              elemento.caracteristicas.map((caracteristica) => "[" + caracteristica + "] ")
+              papel.map((gramaje) => { return gramaje })
             }
-          </MDTypography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <PriceTable
-            table={{columns: columns, rows: rows}}
-            isSorted={false}
-            entriesPerPage={false}
-            showTotalEntries={false}
-            noEndBorder
-          />
-        </AccordionDetails>
-      </Accordion>);
-
-    });
-    lista.push(
-      <MDBox>
-        <MDBox mx={2} mt={3} py={3} px={2}>
-          <MDTypography variant="h6">
-            {title}
-          </MDTypography>
-          {
-            papel.map((gramaje) => gramaje)
-          }
+          </MDBox>
         </MDBox>
-      </MDBox>
-    );
-  });
+      );
+      categoryIndex++;
+    });
+  }
+
+
   return (
-    <Wrapper title="Lista de Precios">
-      {lista}
-    </Wrapper>
+    <div>
+
+      <Wrapper title="Lista de Precios">
+        {
+          lista
+        }
+
+      </Wrapper>
+      <Box sx={{
+        '& > :not(style)': { m: 1 },
+        position: 'fixed',
+        bottom: 16,
+        right: 16,
+      }}>
+        <Fab color="primary" aria-label="add" onClick={() => {
+          if (editing) {
+            //invoke save
+          }
+          setEditing(!editing);
+        }}>
+          {editing ? <SaveIcon /> : <EditIcon />}
+        </Fab>
+      </Box>
+    </div >
+
+
   );
 }
 
