0ee6d795b695fea985c83e867357dda86ba6b67b
Nico Pickelny <npickelny@gmail.com>
2023-06-05 16:17:08 -0300

    fix redirect

--- src/authContexts/ProtectedRoute.jsx
+++ src/authContexts/ProtectedRoute.jsx
@@ -3,13 +3,15 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuthListener } from "./useAuthListener";
 
 export const ProtectedRoute = () => {
-  const { loggedIn } = useAuthListener();
+  const { loggedIn, setCheckingStatus } = useAuthListener();
   return (
     <>
       {
-        loggedIn
-          ? <Outlet />
-          : <Navigate to="/login" />
+        setCheckingStatus
+          ? <div>LOADING </div>
+          : ( loggedIn
+            ? <Outlet />
+            : <Navigate to="/login" />)
       }
     </>
   );
